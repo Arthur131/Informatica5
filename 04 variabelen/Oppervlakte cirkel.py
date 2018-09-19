e26b4d09d94e22e2dac7b3b5e219a9abbb4a867c
Arthur131 <42997642+Arthur131@users.noreply.github.com>
2018-09-19 11:40:42 +0200

    patroontje

---
+++ 04 variabelen/Oppervlakte cirkel.py
@@ -0,0 +1,11 @@
+straal = float(input('geef de straal: '))
+pi = 3.14159
+
+oppervlakte_cirkel = pi * straal * straal
+
+uitvoer = 'De oppervlakte van een cirkel met straal '
+uitvoer += str(straal)
+uitvoer += ' is '
+uitvoer += str(oppervlakte_cirkel)
+
+print(uitvoer)
