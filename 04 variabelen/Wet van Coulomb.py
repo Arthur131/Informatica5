6fc25e54100bc70b25df5c941e28c5f8c5fa537c
Arthur131 <42997642+Arthur131@users.noreply.github.com>
2018-09-22 08:26:33 +0200

    patroontje

---
+++ 04 variabelen/Wet van Coulomb.py
@@ -0,0 +1,10 @@
+r = float(input('geef de afstand: '))
+Q1 = 2.0 * (10 ** (-6))
+Q2 = 1.0 * (10 ** (-6))
+k = 8.99 * (10 ** 13)
+
+coulombkracht = k * ((Q1*Q2) / (r ** 2))
+
+uitvoer = coulombkracht
+
+print(uitvoer)
