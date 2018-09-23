b7789c6b497a05ef433cf9b9919e4c4e7d6c25a4
Arthur131 <42997642+Arthur131@users.noreply.github.com>
2018-09-23 11:49:43 +0200

    Avogadro

---
+++ 04 variabelen/Avogadro.py
@@ -0,0 +1,13 @@
+nS= float(input('aantal deeltjes X: '))
+
+Na = (6.020 * (10 ** 23))
+NS = nS * Na
+
+MS = 32.06
+mS = MS * nS
+
+print(mS)
+print(NS)
+
+
+
---
+++ 04 variabelen/Tranformaties.py
@@ -0,0 +1,10 @@
+a = int(input('waarde van a: '))
+b = int(input('waarde van b: '))
+x = str('x')
+
+f = 2 * (x-3) ** 2 + 4
+
+g = f * (x-a) + b
+
+print('f(x) = ',f)
+print('g(x) = ',g)
