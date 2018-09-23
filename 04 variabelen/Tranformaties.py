c41908b94efe2647b03544331ebd1a91bb329c5b
Arthur131 <42997642+Arthur131@users.noreply.github.com>
2018-09-23 12:09:12 +0200

    Tranformaties

--- 04 variabelen/Tranformaties.py
+++ 04 variabelen/Tranformaties.py
@@ -1,10 +1,9 @@
 a = int(input('waarde van a: '))
 b = int(input('waarde van b: '))
-x = str('x')
+c = 3
+d = 4
+fx = 'f(x) = 2(x - 3)^2 + 4'
+gx = 'g(x) = 2(x - ' + str((c+a)) + ')^2 + ' + str((d+b))
 
-f = 2 * (x-3) ** 2 + 4
-
-g = f * (x-a) + b
-
-print('f(x) = ',f)
-print('g(x) = ',g)
+print(fx)
+print(gx)
