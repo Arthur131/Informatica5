54c7613f914838c5642a69068d71a4857fb699c2
Arthur131 <42997642+Arthur131@users.noreply.github.com>
2018-09-24 22:56:13 +0200

    Wisselgeld

---
+++ 04 variabelen/Wisselgeld.py
@@ -0,0 +1,26 @@
+a = 100
+b = 50
+c = 20
+d = 10
+e = 5
+f = 2
+g = 1
+
+bedrag = int(input('Wat is het bedrag in eurocent? : '))
+
+rest1 = bedrag % a
+rest2 = rest1 % b
+rest3 = rest2 % c
+rest4 = rest3 % d
+rest5 = rest4 % e
+rest6 = rest5 % f
+
+uitkomst1 = bedrag // a
+uitkomst2 = rest1 // b
+uitkomst3 = rest2 // c
+uitkomst4 = rest3 // d
+uitkomst5 = rest4 // e
+uitkomst6 = rest5 // f
+uitkomst7 = rest6 // g
+
+print(bedrag, ' cent kan je omwisselen in '+ str(uitkomst1 + uitkomst2 + uitkomst3 + uitkomst4 + uitkomst5 + uitkomst6 + uitkomst7), ' muntstukken')
